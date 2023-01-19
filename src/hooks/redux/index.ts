b43105b10526265fd870637421cd8ab48984ed2e
MatyZalazar <mzx631@gmail.com>
2023-01-18 21:22:50 -0300

    adding setup for redux toolkit

---
+++ src/hooks/redux/index.ts
@@ -0,0 +1,6 @@
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
+
+import { RootState, Dispatch } from 'redux/store';
+
+export const useCustomDispatch: () => Dispatch = useDispatch;
+export const useCustomSelector: TypedUseSelectorHook<RootState> = useSelector;
